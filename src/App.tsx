d887865adbaf24b94f8014ec65a992ee576150c9
jxxt22 <joce.xxt22@gmail.com>
2025-01-19 18:24:45 -0500

    responsive navbar!!!

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Contact from './Contact';
+import NavBar from './navbar/NavBar';
+function App() {
+
+  return (
+    <>
+    <NavBar/>
+    <Contact/>
+    </>
+  )
+}
+
+export default App;
